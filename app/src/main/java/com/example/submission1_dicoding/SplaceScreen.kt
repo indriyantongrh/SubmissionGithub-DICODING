9e7a499f6a5ac4a98c2bf8a57064c2416131677c
Indriyanto Nugroho <masyantoya@gmail.com>
2020-10-23 11:14:12 +0700

    Initial Commit add hide progressbar

--- app/src/main/java/com/example/submission1_dicoding/SplaceScreen.kt
+++ app/src/main/java/com/example/submission1_dicoding/SplaceScreen.kt
@@ -2,13 +2,15 @@ package com.example.submission1_dicoding
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 
 class SplaceScreen : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splace_screen2)
-
 
         val timerThread: Thread = object : Thread() {
             override fun run() {
